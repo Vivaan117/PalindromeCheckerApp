6183131ef97fbf0e6c5df40ecd525f2054cf8056
Vivaan117 <vs9227@srmist.edu.in>
2026-03-05 10:16:26 +0530

    UC12: Strategy Pattern for Palindrome Algorithms (Advanced)

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -5,31 +5,70 @@ public class PalindromeCheckerApp {
     public static void main(String[] args) {
         System.out.println("Welcome Palindrome");
 
-        System.out.println("Enter a string: ");
         Scanner sc = new Scanner(System.in);
+        System.out.println("Choose Palindrome Strategy:");
+        System.out.println("1. Stack Based");
+        System.out.println("2. Deque Based");
+        System.out.print("Enter choice: ");
+
+        int choice = sc.nextInt();
+        sc.nextLine();
+        System.out.print("Enter string: ");
         String input = sc.nextLine();
 
-        PalindromeChecker checker = new PalindromeChecker();
-        boolean palindrome = checker.checkPalindrome(input);
+        PalindromeStrategy strategy;
 
-        System.out.println("Is Palindrome? : " + palindrome);
-    }
-}
-class PalindromeChecker {
-
-    // Exposed method
-    public boolean checkPalindrome(String input) {
-
-        if (input == null) {
-            return false;
+        if (choice == 1) {
+            strategy = new StackStrategy();
+        } else {
+            strategy = new DequeStrategy();
         }
 
-        // Normalize (remove special characters and make lowercase)
-        String normalized = input.replaceAll("\\s+", "").toLowerCase();
+        boolean result = strategy.isPalindrome(input);
 
-        for (int i = 0; i < normalized.length() / 2; i++) {
-            if (normalized.charAt(i) !=
-                    normalized.charAt(normalized.length() - 1 - i)) {
+        System.out.println("Is palindrome ? : "+ result);
+    }
+}
+
+interface PalindromeStrategy {
+    boolean isPalindrome(String input);
+}
+
+class StackStrategy implements PalindromeStrategy {
+    private String normalize(String input) {
+        return input.replaceAll("\\s+", "").toLowerCase();
+    }
+    public boolean isPalindrome(String input) {
+
+        String normalized = normalize(input);
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : normalized.toCharArray()) {
+            stack.push(c);
+        }
+        for (char c : normalized.toCharArray()) {
+            if (c != stack.pop()) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+class DequeStrategy implements PalindromeStrategy {
+    private String normalize(String input) {
+        return input.replaceAll("\\s+", "").toLowerCase();
+    }
+    public boolean isPalindrome(String input) {
+
+        String normalized = normalize(input);
+        Deque<Character> deque = new ArrayDeque<>();
+
+        for (char c : normalized.toCharArray()) {
+            deque.addLast(c);
+        }
+        while (deque.size() > 1) {
+            if (!deque.removeFirst().equals(deque.removeLast())) {
                 return false;
             }
         }
